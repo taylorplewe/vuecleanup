6b24f3d10f45e51af9dfb5531dd510350c3b3401
Taylor Plewe <taylor.plewe@awardco.com>
2022-07-02 22:29:40 -0600

    all types of unused labels are functional, checking file as you type and when you switch between files, tree looks great.

--- src/extension.ts
+++ src/extension.ts
@@ -39,6 +39,8 @@ function registerEvents() {
 
 function updateUnusedLabelsData() {
 	if (vscode.window.activeTextEditor) {
+		console.log(vscode.window.activeTextEditor.document.fileName);
+		// console.log(vscode.window.activeTextEditor.document.languageId);
 		fileLabelsData.updateData(checkForUnusedLabels(vscode.window.activeTextEditor.document));
 		fileLabelsData.refresh();
 	}
--- src/unused-labels/unusedLabelsCheck.ts
+++ src/unused-labels/unusedLabelsCheck.ts
@@ -3,21 +3,34 @@ import * as vscode from 'vscode';
 import UNUSED_LABELS_TYPES from './unusedLabelsTypes';
 
 export default function checkForUnusedLabels(doc: vscode.TextDocument): any {
-	const fileText: string = doc.getText();
+	const fileText: string = removeCommentsFromText(doc.getText());
 	const varsInFile: Set<string> = getVarsInFile(fileText);
 	const cssClassesInFile: Set<string> = getCssClassesInFile(fileText);
-	console.log(cssClassesInFile);
+	const dataMembersInFile: Set<string> = getDataMembersInFile(fileText);
+	const computedsInFile: Set<string> = getKeysOfNestedObject(fileText, 'computed');
+	const methodsInFile: Set<string> = getKeysOfNestedObject(fileText, 'methods');
 
 	let labelsWithOccurrences: any = buildLabelsWithOccurrences();
 
-	const scriptSectionNoCommentsOrStrings = getScriptSectionNoCommentsOrStrings(fileText);
+	const scriptSectionNoCommentsOrStrings = getFileSectionByType(fileText, 'script', true);
+	console.log(scriptSectionNoCommentsOrStrings);
 	varsInFile.forEach(v => {
+		// NOTE I should be able to search for variables in their own scope instead of doing this
 		labelsWithOccurrences.variables[v] =
-			getOccurrencesOfVariable(scriptSectionNoCommentsOrStrings, v);
+			getOccurrencesOfAnyPropertyInText(scriptSectionNoCommentsOrStrings, v);
 	});
 	cssClassesInFile.forEach(c => {
 		labelsWithOccurrences.cssClasses[c] = getOccurrencesOfCssClasses(fileText, c.substring(1)) + 1;
 	});
+	dataMembersInFile.forEach(d => {
+		labelsWithOccurrences.dataMembers[d] = getOccurrencesOfAnyPropertyInText(fileText, d);
+	});
+	computedsInFile.forEach(c => {
+		labelsWithOccurrences.computeds[c] = getOccurrencesOfAnyPropertyInText(fileText, c);
+	});
+	methodsInFile.forEach(m => {
+		labelsWithOccurrences.methods[m] = getOccurrencesOfAnyPropertyInText(fileText, m);
+	})
 
 	return labelsWithOccurrences;
 }
@@ -31,27 +44,20 @@ function buildLabelsWithOccurrences(): any {
 }
 
 function getVarsInFile(fileText: string): Set<string> {
-	const variableRegex: RegExp = /(?<=\s+(var|let|const)\s+)(\w+)/g;
-	const variables = fileText.match(variableRegex);
+	const variablesRegex: RegExp = /(?<=\s+(var|let|const)\s+)(\w+)/g;
+	const variablesMatch: string[] | null = fileText.match(variablesRegex);
+	const variables: string[] = variablesMatch ? variablesMatch : [];
 	return new Set(variables);
-}
-function getOccurrencesOfVariable(text: string, varName: string): number {
-	let occurrences: number | undefined = 0;
-
-	const variableRegex: RegExp = new RegExp(`\\W${varName}\\W`, 'g');
-	occurrences = text.match(variableRegex)?.length;
-
-	return occurrences ? occurrences: 0;
 }
 
 function getCssClassesInFile(fileText: string): Set<string> {
-	const fileStyleSection = getFileSectionByType(fileText, 'style');
+	const fileStyleSection = getFileSectionByType(fileText, 'style', false);
 	const searchCssClassesRegex: RegExp = /\B\.[\w\-_]+/g;
 	const cssClasses = fileStyleSection.match(searchCssClassesRegex);
 	return new Set(cssClasses);
 }
 function getOccurrencesOfCssClasses(fileText: string, className: string): number {
-	const fileTemplateSection: string = getFileSectionByType(fileText, 'template');
+	const fileTemplateSection: string = getFileSectionByType(fileText, 'template', false);
 	const searchClassInardsRegex: RegExp = /(?<=\bclass=").*?(?=")/gs;
 	const searchClassNameRegex: RegExp = new RegExp(`\\b${className}\\b`, 'gs');
 	
@@ -68,22 +74,54 @@ function getOccurrencesOfCssClasses(fileText: string, className: string): number
 
 function getDataMembersInFile(fileText: string): Set<string> {
 	const getDataSectionRegex: RegExp =
-		/(?:data\s*?\(\s*?\)\s*?\{)((?:[^\{]|\{.*?\})*?)(?=\})/gs;
+		/(?:data\s*?\(\s*?\)\s*?\{.*?return\s*?\{)(([^\{]|\{.*?\})*?)(?=\})/s;
+	const getDataMemberNamesRegex: RegExp =
+		/(?:\s*.*?)(\w+)(?:(?:[^\{\[]|\{.*?\}|\[.*?\])*?(?:\,|$))/gsy;
 
-	let dataMembers: string[] = [];
 	const fileDataSectionMatch: string[] | null = fileText.match(getDataSectionRegex);
 	const fileDataSection: string = fileDataSectionMatch ? fileDataSectionMatch[1] : '';
-	
+	const dataNamesMatch: RegExpMatchArray[] = [...fileDataSection.matchAll(getDataMemberNamesRegex)];
+	const dataNames: string[] = dataNamesMatch.map(n => n[1]);
 
-	return new Set(dataMembers);
+	return new Set(dataNames);
+}
+// handles searching for variables, computed properties, data members
+function getOccurrencesOfAnyPropertyInText(text: string, property: string): number {
+	const allButStyleRegex: RegExp = /.*?(?=<\s*style|$)/s;
+	const searchPropertyRegex: RegExp = new RegExp(`\\b${property}\\b`, 'gs');
+
+	const allButStyleMatch: string[] | null = text.match(allButStyleRegex);
+	const allButStyle: string = allButStyleMatch ? allButStyleMatch[0] : '';
+	const propertyMatch: string[] | null = allButStyle.match(searchPropertyRegex);
+	return propertyMatch ? propertyMatch.length : 0;
 }
 
-function getScriptSectionNoCommentsOrStrings(fileText: string): string {
-	let fileScriptSection: string = getFileSectionByType(fileText, 'script');
-	fileScriptSection = removeCommentsFromText(fileScriptSection);
-	fileScriptSection = removeStringsFromText(fileScriptSection);
-	return fileScriptSection;
+function getKeysOfNestedObject(fileText: string, objectName: string): Set<string> {
+	const objectInards = getNestedObjectInards(fileText, objectName);
+	const getFirstWhitespaceRegex: RegExp = /^.*?(?=\w)/m;
+	const firstWhitespace: any = objectInards.match(getFirstWhitespaceRegex)?.at(0);
+
+	const getKeysRegex: RegExp = new RegExp(
+		`(?:^${firstWhitespace})(\\w+)(?=\\s*?\\(.*?\\)\\s*?\\{)`, 'gm'
+	);
+	const keysMatch: RegExpMatchArray[] = [...objectInards.matchAll(getKeysRegex)];
+	const keys: string[] = keysMatch.map(m => m[1]);
+	return new Set(keys);
 }
+
+// {objectName}: { ... } -> returns ...
+function getNestedObjectInards(fileText: string, objectName: string): string {
+	const getFirstWhitespaceRegex: RegExp = new RegExp(
+		`\\n\\s*?(?=${objectName}\\s*?:\\s*?\\{)`, 's'
+	);
+	const firstWhitespace: any = fileText.match(getFirstWhitespaceRegex)?.at(0);
+	const getInardsRegex: RegExp = new RegExp(
+		`(?:\\s*?${objectName}\\s*?:\\s*?\\{)(.*?)(?=${firstWhitespace}\\})`, 's'
+	);
+	const inardsMatch: RegExpMatchArray | null = fileText.match(getInardsRegex);
+	return inardsMatch ? inardsMatch[1] : '';
+}
+
 function removeCommentsFromText(text: string): string {
 	const searchCommentRegex: RegExp = /\/\/[^\n]*|\/\*.*?\*\//gs;
 	return text.replace(searchCommentRegex, '');
@@ -118,10 +156,14 @@ function formatEscapesForStringAppend(escapes: string[]): string {
 	}, formattedEscapesString);
 }
 
-function getFileSectionByType(fileText: string, type: string): string {
+function getFileSectionByType(fileText: string, type: string, removeStrings: boolean): string {
 	const getSectionRegex: RegExp = new RegExp(
 		`(?:<\\s*?${type}[\\s\\S]*?>)([\\s\\S]*?)(?=<\\/\\s*${type})`
 	);
 	const sectionMatch = fileText.match(getSectionRegex);
-	return sectionMatch ? sectionMatch[1]: '';
+	if (removeStrings && sectionMatch)
+		return removeStringsFromText(sectionMatch[1])
+	else if (sectionMatch)
+		return sectionMatch[1];
+	else return '';
 }
