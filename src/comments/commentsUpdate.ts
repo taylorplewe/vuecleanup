c3e0dc672fbe2a33187048abaea49d6d9997b9cb
Taylor Plewe <taylor.plewe@awardco.com>
2022-07-05 19:15:25 -0600

    comments filter out todos

--- src/comments/commentsUpdate.ts
+++ src/comments/commentsUpdate.ts
@@ -3,6 +3,14 @@ import * as vscode from 'vscode';
 export default function getCommentsData(fileText: string): string[] {
     const searchCommentsRegex: RegExp = /\/\/[^\n]*?(\S.*)|\/\*[\s]*((?:[\s\S])*?)(?=\*\/)/gmi;
     const commentsMatch: RegExpMatchArray[] | null = [...fileText.matchAll(searchCommentsRegex)];
-    const comments: string[] = commentsMatch.map(t => t[1] ? t[1] : t[2]);
+    let comments: string[] = commentsMatch.map(t => t[1] ? t[1] : t[2]);
+
+    comments = filterOutTodosFromComments(comments);
+
     return comments;
+}
+
+function filterOutTodosFromComments(comments: string[]): string[] {
+    const filterTodosRegex = /^todo/i;
+    return comments.filter(c => !c.match(filterTodosRegex));
 }
